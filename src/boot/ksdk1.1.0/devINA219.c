85addac5061cc51a403190f8228268ed90601b33
RoryGordon <55985626+RoryGordon@users.noreply.github.com>
2020-11-23 16:59:11 +0000

    another attempt at printing

--- src/boot/ksdk1.1.0/devINA219.c
+++ src/boot/ksdk1.1.0/devINA219.c
@@ -155,7 +155,7 @@ readSensorRegisterINA219(uint8_t deviceRegister, int numberOfBytes)
 							cmdBuf,
 							1,
 							(uint8_t *)deviceINA219State.i2cBuffer,
-							1,
+							numberOfBytes,
 							gWarpI2cTimeoutMilliseconds);
 
 	if (status != kStatus_I2C_Success)
