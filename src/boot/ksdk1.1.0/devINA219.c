16bae12fb9d684be43e4e9c96917345f6caafb19
RoryGordon <55985626+RoryGordon@users.noreply.github.com>
2020-11-24 20:47:55 +0000

    converting voltage

--- src/boot/ksdk1.1.0/devINA219.c
+++ src/boot/ksdk1.1.0/devINA219.c
@@ -218,7 +218,7 @@ printSensorDataINA219(bool hexModeFlag)
 		else
 		{
 			SEGGER_RTT_printf(0,
-							  " %d.%d, %d.%d",
+							  " %d.%01d, %d.%02d",
 							  abs(readSensorRegisterValueCombined/10),
 							  abs(readSensorRegisterValueCombined%10),
 							  abs(readSensorRegisterValueCombined/100),
