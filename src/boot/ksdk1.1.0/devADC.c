315b0fae9910cc1ab5d720033f5359b4c217e36a
RoryGordon <55985626+RoryGordon@users.noreply.github.com>
2021-01-10 14:56:31 +0000

    culling everything unneccessary

--- src/boot/ksdk1.1.0/devADC.c
+++ src/boot/ksdk1.1.0/devADC.c
@@ -41,7 +41,7 @@
 #define UPPER_VALUE_LIMIT       (1U)        /*! This value/10 is going to be added to current Temp to set the upper boundary*/
 #define LOWER_VALUE_LIMIT       (1U)        /*! This Value/10 is going to be subtracted from current Temp to set the lower boundary*/
 #define UPDATE_BOUNDARIES_TIME  (20U)       /*! This value indicates the number of cycles needed to update boundaries. To know the Time it will take, multiply this value times LPTMR_COMPARE_VALUE*/
-#define kAdcChannelADC0_SE0     (0U)
+#define kAdcChannelADC0_SE3     (3U)
 #define kAdcChannelTemperature  (26U)       /*! ADC channel of temperature sensor */
 #define kAdcChannelBandgap      (27U)       /*! ADC channel of BANDGAP */
 
@@ -215,7 +215,7 @@ static int32_t initADC(uint32_t instance)
     // Install Callback function into ISR
     ADC_TEST_InstallCallback(instance, CHANNEL_0, ADC1IRQHandler);
     
-    adcChnConfig.chnNum = CHANNEL_0;
+    adcChnConfig.chnNum = kAdcChannelADC0_SE3;
     //adcChnConfig.chnNum = kAdcChannelBandgap;
     adcChnConfig.diffEnable = false;
     adcChnConfig.intEnable = false; // maybe set to false (originally true)?
