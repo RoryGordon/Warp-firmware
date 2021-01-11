db8ea539a264813fa1e0663217ec6cba0693c8ed
RoryGordon <55985626+RoryGordon@users.noreply.github.com>
2021-01-11 16:01:33 +0000

    adding delay scheme

--- src/boot/ksdk1.1.0/warp-kl03-ksdk1.1-boot.c
+++ src/boot/ksdk1.1.0/warp-kl03-ksdk1.1-boot.c
@@ -1337,15 +1337,17 @@ main(void)
 				for(int16_t i = 0; i < 10*delayBufSize; i++)
 				{
 					delayBuffer[writePos] = inputSignal + feedback;
-					delayOut = (delayBuffer[readPos]*Gain_d) >> Gain_div_d;
+					delayOut = ((delayBuffer[readPos]*Gain_d) >> Gain_div_d)&0xFFF;
 					outputSignal = delayOut + inputSignal;
 					
 					// For some weird reason this line completely breaks everything
 					//SEGGER_RTT_printf(0, "%6d", outputSignal);
-					SEGGER_RTT_WriteString(0,"Will this print?\n");
-
+					if(outputSignal != 0)
+					{
+						SEGGER_RTT_WriteString(0,"Will this print?\n");
+					}
 					
-					feedback = (outputSignal*Gain_f) >> Gain_div_f;
+					feedback = ((outputSignal*Gain_f) >> Gain_div_f)&0xFFF;
 
 					writePos = (writePos+1) % delayBufSize;
 					readPos = (readPos+1) % delayBufSize;
