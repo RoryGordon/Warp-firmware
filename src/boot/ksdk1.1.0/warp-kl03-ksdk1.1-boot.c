1828a3fd8625bd99aeb99074371503b970e5f128
RoryGordon <55985626+RoryGordon@users.noreply.github.com>
2021-01-11 18:41:23 +0000

    adding delay scheme

--- src/boot/ksdk1.1.0/warp-kl03-ksdk1.1-boot.c
+++ src/boot/ksdk1.1.0/warp-kl03-ksdk1.1-boot.c
@@ -1334,29 +1334,31 @@ main(void)
 
 				}
 				
-				delayBuffer[15] = 2048; // Imitating a single pulse input
-				SEGGER_RTT_printf(0, "%d\n", delayBuffer[15]);
-				SEGGER_RTT_printf(0, "%d\n", delayBuffer[readPos]);
+				//delayBuffer[15] = 2048; // Imitating a single pulse input
+				//SEGGER_RTT_printf(0, "%d\n", delayBuffer[15]);
+				//SEGGER_RTT_printf(0, "%d\n", delayBuffer[readPos]);
 
 				SEGGER_RTT_WriteString(0,"\tBegin here\n");
 				for(int16_t i = 0; i < 10*delayBufSize; i++)
 				{
+					if(i%5 == 0)
+					{
+						inputSignal = 2048;
+					}
+					else
+					{
+						inputSignal = 0;
+					}
+					
 					delayBuffer[writePos] = (inputSignal + feedback)&0xFFF;
-					//delayBuffer[writePos] = inputSignal;
+					
 					delayOut = ((delayBuffer[readPos]*Gain_d) >> Gain_div_d)&0xFFF;
-					//delayOut = delayBuffer[readPos];
-					//outputSignal = (delayOut + inputSignal)&0xFFF;
-					outputSignal = delayOut;
-					// For some weird reason this line completely breaks everything
+					
+					outputSignal = (delayOut + inputSignal)&0xFFF;
+
 					SEGGER_RTT_printf(0, "writePos: %6d (%6d) | readPos: %6d (%6d)\n",
 						writePos, delayBuffer[writePos], readPos, outputSignal);
-					// So does this statement - seems to be references to outputSignal
-					/*
-					if(outputSignal != 0)
-					{
-						SEGGER_RTT_WriteString(0,"Will this print?\n");
-					}
-					*/
+
 					feedback = ((outputSignal*Gain_f) >> Gain_div_f)&0xFFF;
 
 					writePos = (writePos+1) % delayBufSize;
