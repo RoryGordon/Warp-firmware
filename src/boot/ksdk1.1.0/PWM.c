43c8ccaa296dfd68973f0bd813412ff072f2d4cd
RoryGordon <55985626+RoryGordon@users.noreply.github.com>
2021-01-13 02:01:46 +0000

    first test of PWM

--- src/boot/ksdk1.1.0/PWM.c
+++ src/boot/ksdk1.1.0/PWM.c
@@ -52,8 +52,9 @@ void writeToPWM(uint16_t output)
     if(TPM_DRV_PwmStart(TPM_0, &PwmParams, PWM_CHANNEL))
     {
         //TPM_DRV_SetTimeOverflowIntCmd(TPM_0, true);
-        SEGGER_RTT_printf(0, "\tfreq: %d, Input val: %3d, Channel val: %3d\n",
-            TPM_DRV_GetClock(TPM_0), PwmParams.uDutyCyclePercent, TPM_DRV_GetChnVal(TPM_0, PWM_CHANNEL));
+        SEGGER_RTT_printf(0, "\tfreq: %d, Input val: %3d, Channel val: %4d, mod val: %4d\n",
+            TPM_DRV_GetClock(TPM_0), PwmParams.uDutyCyclePercent,
+            TPM_DRV_GetChnVal(TPM_0, PWM_CHANNEL), TPM_DRV_GetMod(g_tpmBaseAddr[TPM_0]));
     }
     else
     {
