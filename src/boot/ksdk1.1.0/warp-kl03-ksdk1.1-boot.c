eba56493af0c822e0a71e91a9f5d36af2c77d8db
RoryGordon <55985626+RoryGordon@users.noreply.github.com>
2021-01-13 00:26:34 +0000

    first test of PWM

--- src/boot/ksdk1.1.0/warp-kl03-ksdk1.1-boot.c
+++ src/boot/ksdk1.1.0/warp-kl03-ksdk1.1-boot.c
@@ -524,14 +524,14 @@ lowPowerPinStates(void)
 	 *	NOTE: The KL03 has no PTB8 or PTB9
 	 */
 
-	PORT_HAL_SetMuxMode(PORTB_BASE, 10, kPortMuxAsGpio);
-	PORT_HAL_SetMuxMode(PORTB_BASE, 11, kPortMuxAsGpio);
+	//PORT_HAL_SetMuxMode(PORTB_BASE, 10, kPortMuxAsGpio);
+	//PORT_HAL_SetMuxMode(PORTB_BASE, 11, kPortMuxAsGpio);
 
 	/*
 	 *	NOTE: The KL03 has no PTB12
 	 */
 
-	PORT_HAL_SetMuxMode(PORTB_BASE, 13, kPortMuxAsGpio);
+	//PORT_HAL_SetMuxMode(PORTB_BASE, 13, kPortMuxAsGpio);
 
 
 
@@ -1443,7 +1443,9 @@ main(void)
 
 				// Set green LED as output
 				//PORT_HAL_SetMuxMode(PORTB_BASE,10u,kPortMuxAlt2);
-				PORT_HAL_SetMuxMode(PORTB_BASE,11u, kPortPinDisabled);
+				//PORT_HAL_SetMuxMode(PORTB_BASE,11u, kPortPinDisabled);
+				//PORT_HAL_SetMuxMode(PORTB_BASE,11u, kPortPinAlt2);
+
 				OSA_TimeDelay(500); // Desperately hoping this gives it time or smth
 				SEGGER_RTT_WriteString(0, "\nInitialising PWM...\n");
 				initPWM();
